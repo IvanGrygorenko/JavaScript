15e9124fbfa2d5119e36f9d2d27a5bee26f66031
Ivan Grygorenko <151570701+IvanGrygorenko@users.noreply.github.com>
2024-02-05 18:41:22 +0200

    Add files via upload

---
+++ lesson2/main.js
@@ -0,0 +1,30 @@
+let num = 99
+let str = 'Ivan'
+let bool = true
+let und = undefined
+let nu = null
+
+let a;
+console.log(und)
+
+
+
+
+
+console.log(Boolean(6 > 5))
+console.log(typeof 43)
+console.log(typeof '43')
+
+console.log(str.includes("Iv"))
+console.log(str.toLocaleLowerCase())
+
+
+let obj = {
+    name: "iphone",
+    price: 1300,
+    model: "15 pro max"
+}
+
+let arr = [1, "dwdwdw", true, 4]
+
+console.log(typeof arr)
